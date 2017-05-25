b03c1c665f52ac20ecd16f771b044f8105ead6bd
yh2n <yohann.potico@gmail.com>
2017-05-25 16:31:43 -0400

    fix last page issue

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ state = {
 	 	  correctAnswer: "Bill Connors",
 	 	  userAnswer: " "},
 
-	 	{ question: "What classically trained pianist/composer also poet, deeply influenced by dancers\
-	 	  			  is one of the pioneer of free jazz?",
+	 	{ question: "This classically trained pianist/composer and poet, deeply influenced by dancers\
+	 	  			  is one of the pioneer of free jazz...",
 	 	  choice1: "Muhal Richard Abrams", choice2: "Cecil Taylor", choice3: "Keith Jarrett", choice4: "Andrew Hill",
 	 	  correctAnswer: "Cecil Taylor",
 	 	  userAnswer: " "},
@@ -43,7 +43,7 @@ state = {
 	 	  correctAnswer: "Hubert Laws",
 	 	  userAnswer: " "},
 
-	 	{ question: "What pianist/vovalist of Trinidadian descent, child prodigy has been featured\
+	 	{ question: "What pianist/vocalist of Trinidadian descent, child prodigy has been featured\
 	 				 in films, showcasing her virtuosity?",
 	 	  choice1: "Rhoda Scott", choice2: "Hazel Scott", choice3: "Geri Allen", choice4: "Lil Hardin Armstrong",
 	 	  correctAnswer: "Hazel Scott",
@@ -55,10 +55,10 @@ state = {
  		  correctAnswer: "Keith Jarrett",
  		  userAnswer: " " },
 
-	 	{ question: "Who composed the soundtrack for the French movie 'Ascenseur pour l'échaffaud'?",
+	 	{ question: "Who composed the soundtrack for the French movie 'Ascenseur pour l'Echaffaud'?",
 	 	  choice1: "Miles Davis", choice2: "Duke Ellington", choice3: "Dizzie Gillespie", choice4: "Charles Mingus",
 	 	  correctAnswer: "Miles Davis",
-	 	  userAnswer: " "}],			
+	 	  userAnswer: " "}],	
 
 	score: 0,
 	progress: 0,
@@ -75,81 +75,81 @@ var page = state.page;
 
 //3. Rendering
 
-function addContent() {
+function addContent(state) {
 	$(".question").text(currentQuestion.question);
 	$(".choice_1").text(currentQuestion.choice1);
 	$(".choice_2").text(currentQuestion.choice2);
 	$(".choice_3").text(currentQuestion.choice3);
 	$(".choice_4").text(currentQuestion.choice4);
 	$(".choice_1, .choice_2, .choice_3, .choice_4").removeClass("selected");
+	$(".progress").text("Question " + page + "/10").fadeIn(2000);
+	
 }
 
 //.4 Event listener
 
-function userAnswer() {
+function userOptions(state) {
 	$(".choice_1, .choice_2, .choice_3, .choice_4").on("click", function(event) {
 		$(this).addClass("selected").siblings().removeClass("selected");
 	}); 
 }
 
 function submitChoice() {
+			$("#submit").on("click", function(event) {
+			if ($(".selected").text() === "") {
+				alert("pick one first ;)");
+				
+			}
 
-	$("#submit").on("click", function(event) {
-		if ($(".selected").text() === "") {
-			alert("pick one first ;)");
 			
-		}
+			else if ($(".selected").text() === state.questions[progress].correctAnswer) {
+				$("#submit").hide();		  
+				updateScore();
+				$(".verdict").text("Well done!");
+				$(".selected").siblings().addClass("wrong_answers");
+				$(".next").show();
+			 }
 
-		
-		else if ($(".selected").text() === state.questions[progress].correctAnswer) {		  
-			$("#submit").fadeOut(300);
-			updateScore();
-			$(".selected").animate({width: "250px", 
-				height:"80px", 
-				marginLeft: "250px", 
-				fontSize: "25px"}, 
-				500);
-			$(".selected").text("Well done!");
-			$(".selected").siblings().hide();
-			$(".next").show();
-		 }
-
-		else {
-			$("#submit").fadeOut(300);
-			$(".selected").animate({width: "250px", 
-				height:"80px", 
-				marginLeft: "250px", 
-				fontSize: "25px", }, 
-				500);
-			$(".selected").text("Incorrect... it's " + state.questions[progress].correctAnswer);
-			$(".selected").siblings().hide();
-			$(".next").show();
-			
-		}
-	});
+			else {
+				$("#submit").hide();
+				$(".verdict").text("Incorrect... it's " + state.questions[progress].correctAnswer);
+				$(".selected").siblings().addClass("wrong_answers");
+				$(".next").show();
+				}
+			}); 
 }
 
 function updateContent() {
 	$(".next").on("click", function() {
-		$(".selected").hide().animate({width: "150px", 
-				height:"45px", 
-				marginLeft: "0px", 
-				fontSize: "16px"}, 
-				"fast");
+		if(page === 10) {
+			if (score >= 1) {
+				$(".question_panel").hide();
+				$(".final-results").text("You got " + score + " points!");
+			   }
+			 else {
+			 	$(".question_panel").hide();
+			   	$(".final-results").text("You got " + score + " point!");
+			   }
+		}
+
+		else {
+		$(".verdict").text("");
 		$(".selected").show();
-		$(".selected").siblings().show();
+		$(".selected").siblings().removeClass("wrong_answers");
 		updatePage();
 		$("#submit").show();
 		progress++;
 		currentQuestion = state.questions[progress];
 		$(".next").hide();
-		$(".correctAnswer").hide();
 		addContent();
+	   }
 	 });
 }
+
+// Nav
 function updatePage() {
 		page++;
-		$(".progress").text("Question " + page + "/10")
+		$(".progress").text("Question " + page + "/10");
 }
 
 function updateScore() {
@@ -163,25 +163,25 @@ function updateScore() {
 		score++;
 		$(".score").text(score + " point");
 	}
-
-	else {
-		console.log();
-	}
 }
+
+
 
 function start() {
 	$(".question_panel").hide();
+	$(".progress").hide();
 	$(".start").on("click", function() {
-		$("#front_page").fadeOut(700);
-		$(".question_panel").show();
+		$("#front_page").fadeOut(500);
+		$(".question_panel").fadeIn(1000);
 		$(".next").hide();
+		
 	});
 
 	addContent();
-	userAnswer();
+	userOptions();
 	submitChoice();
 	updateContent();
-	addContent();
+	//addContent();
 }
 
 $(function () {
