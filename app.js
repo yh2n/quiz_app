8548aaf35cfbcc7c47fe33459bd41bc1dbd6a918
Mark Nyon <mark@grandkru.com>
2017-09-13 15:42:15 -0400

    Revert "fix comments, pix, verdict page"
    This reverts commit c228a74f0857d67662b523b176e1024a2a52339a.

--- app.js
+++ app.js
@@ -1,74 +1,64 @@
- state = {
+state = {
  	questions: [
 
  		{ question: "What bass player played an unconventional rendition\
-	 				 of 'Donna Lee' on his first album?",
+	 				 of Donna Lee on his first album?",
 	 	  choice1: "Alphonso Johnson", choice2: "Miroslav Vitous", choice3: "Jaco Pastorius", choice4: "Stanley Clarke",
 	 	  correctAnswer: "Jaco Pastorius",
-	 	  correctAnswerText: "Great start!",
-	 	  wrongAnswerText: "Nope... Jaco, the one and only. That was easy..."},
+	 	  userAnswer: " "},
 
 	 	{ question: "What pianist, part of a famous jazz-fusion band, wrote the\
-	 				 opening theme for the TV show 'Miami Vice'?" ,
+	 				 opening theme for the TV show Miami Vice?" ,
 	 	 choice1: "George Duke", choice2: "Jan Hammer", choice3: "Joe Zawinul", choice4: "Chick Corea",
 	 	 correctAnswer: "Jan Hammer",
-	 	 correctAnswerText: "There you go!",
-	 	 wrongAnswerText: "Absolutely not! It's Jan Hammer! Mahavishnu Orchestra, remember?" },
+	 	 userAnswer: " " },
 
 	 	{ question: "Who was nicknamed \"Sassy\" ?",
 	 	  choice1: "Diana Washington", choice2:  "Ella Fitzgerald", choice3: "Betty Carter", 
 	 	  	choice4: "Sarah Vaughan",
 	 	  correctAnswer: "Sarah Vaughan",
-	 	  correctAnswerText: "Yup, that's her...",
-	 	  wrongAnswerText: "Sarah Vaughan. Shame on you!" },
+	 	  userAnswer: " "},
 
-	 	{ question: "He is the first guitar player to join Return To Forever?",
+	 	{ question: "Who was the first guitar player to join Return To Forever?",
 	 	  choice1: "Bill Connors", choice2: "'BlackByrd' McKnight", choice3: "Frank Gambale", 
 	 	  	choice4: "Al Di Meola",
 	 	  correctAnswer: "Bill Connors",
-	 	  correctAnswerText: "Impressive! That was a hard one.",
-	 	  wrongAnswerText: "Bill Connors. You should check 'The Game Maker'..."},
+	 	  userAnswer: " "},
 
 	 	{ question: "This classically trained pianist/composer and poet, deeply influenced by dancers\
 	 	  			  is one of the pioneer of free jazz...",
 	 	  choice1: "Muhal Richard Abrams", choice2: "Cecil Taylor", choice3: "Keith Jarrett", choice4: "Andrew Hill",
 	 	  correctAnswer: "Cecil Taylor",
-	 	  correctAnswerText: "Good job!",
-	 	  wrongAnswerText: "' was tricky. It's Cecil Taylor."},
+	 	  userAnswer: " "},
 
 	 	{ question: "Following John Coltrane's legacy, this avant-garde tenor player blended African chants\
 	 				and polyrythm with improvisation?",
 	 	  choice1: "Roscoe Mitchell", choice2: "David Murray", choice3: "Eric Dolphy", choice4: "Pharoah Sanders",
 	 	  correctAnswer: "Pharoah Sanders",
-	 	  correctAnswerText: "Nicely done!",
-	 	  wrongAnswerText: "Pharoah Sanders...pleeease..."},
+	 	  userAnswer: " " },
 
 	 	{ question: "What flautist was heavily featured on Gil Scott-Heron album\
 	 				 'Pieces of a Man'?",
 	 	  choice1: "Bobbi Humphrey", choice2: "Hubert Laws", choice3: "Herbie Mann", choice4: "Frank Wess",
 	 	  correctAnswer: "Hubert Laws",
-	 	  correctAnswerText: "Yeaah!",
-	 	  wrongAnswerText: "Come ooon... Hubert Laws!"},
+	 	  userAnswer: " "},
 
-	 	{ question: "She's a pianist/vocalist of Trinidadian descent, child prodigy, featured\
-	 				 in films playing two pianos at once?",
+	 	{ question: "What pianist/vocalist of Trinidadian descent, child prodigy has been featured\
+	 				 in films, showcasing her virtuosity?",
 	 	  choice1: "Rhoda Scott", choice2: "Hazel Scott", choice3: "Geri Allen", choice4: "Lil Hardin Armstrong",
 	 	  correctAnswer: "Hazel Scott",
-	 	  correctAnswerText: "That's right!",
-	 	  wrongAnswerText: "Hazel Scott. You gotta look her up!"},
+	 	  userAnswer: " "},
 
 	 	{ question: "What pianist played electric piano -though reluctantly- on Miles Davis recording\
 	 				 'Live at the Fillmore East?",
 	 	  choice1: "Keith Jarrett", choice2: "Joe Zawinul", choice3: "Herbie Hanccock", choice4: "Bill Evans",
  		  correctAnswer: "Keith Jarrett",
- 		  correctAnswerText: "Yup",
-	 	  wrongAnswerText: "Nooo, Keith Jarrett. You could have guessed that one..."},
+ 		  userAnswer: " " },
 
 	 	{ question: "Who composed the soundtrack for the French movie 'Ascenseur pour l'Echaffaud'?",
 	 	  choice1: "Miles Davis", choice2: "Duke Ellington", choice3: "Dizzie Gillespie", choice4: "Charles Mingus",
 	 	  correctAnswer: "Miles Davis",
-	 	  correctAnswerText: "Great job...",
-	 	  wrongAnswerText:"Reaaally? You missed that!? It's Miles Davis."}],	
+	 	  userAnswer: " "}],	
 
 	score: 0,
 	progress: 0,
@@ -87,12 +77,13 @@ var page = state.page;
 
 function addContent(state) {
 	$(".question").text(currentQuestion.question);
-	$("#choice_1").text(currentQuestion.choice1);
-	$("#choice_2").text(currentQuestion.choice2);
-	$("#choice_3").text(currentQuestion.choice3);
-	$("#choice_4").text(currentQuestion.choice4);
-	$(".choice").removeClass("selected");
-	$(".progress").text("Question " + page + "/10").show();
+	$(".choice_1").text(currentQuestion.choice1);
+	$(".choice_2").text(currentQuestion.choice2);
+	$(".choice_3").text(currentQuestion.choice3);
+	$(".choice_4").text(currentQuestion.choice4);
+	$(".choice_1, .choice_2, .choice_3, .choice_4").removeClass("selected");
+	$(".progress").text("Question " + page + "/10").fadeIn(2000);
+	
 }
 
 //.4 Event listener
@@ -104,9 +95,9 @@ function userOptions(state) {
 }
 
 function submitChoice() {
-	$("#submit").on("click", function(event) {
+			$("#submit").on("click", function(event) {
 			if ($(".selected").text() === "") {
-				alert("pick one first;)");
+				alert("pick one first ;)");
 				
 			}
 
@@ -114,31 +105,30 @@ function submitChoice() {
 			else if ($(".selected").text() === state.questions[progress].correctAnswer) {
 				$("#submit").hide();		  
 				updateScore();
-				$(".verdict").text(state.questions[progress].correctAnswerText);
+				$(".verdict").text("Well done!");
 				$(".selected").siblings().addClass("wrong_answers");
 				$(".next").show();
 			 }
 
 			else {
 				$("#submit").hide();
-				$(".verdict").text(state.questions[progress].wrongAnswerText);
+				$(".verdict").text("Incorrect... it's " + state.questions[progress].correctAnswer);
 				$(".selected").siblings().addClass("wrong_answers");
 				$(".next").show();
 				}
 			}); 
 }
 
-
 function updateContent() {
 	$(".next").on("click", function() {
 		if(page === 10) {
-			$(".question_panel").hide();
-			$(".background_image").addClass("background_image_questions");
-			if (score > 1) {
-				$(".final-results").append("You got " + score + " points!\nHappy?!");
-				}
+			if (score >= 1) {
+				$(".question_panel").hide();
+				$(".final-results").text("You got " + score + " points!");
+			   }
 			 else {
-			   	$(".final-results").text("You got " + score + " point!\nOuch!");
+			 	$(".question_panel").hide();
+			   	$(".final-results").text("You got " + score + " point!");
 			   }
 		}
 
@@ -172,22 +162,17 @@ function updateScore() {
 	else if ($(".selected").text() === state.questions[progress].correctAnswer) {
 		score++;
 		$(".score").text(score + " point");
-	};
+	}
 }
 
-// function reset() {
-// 	$(".reset").on("click", function() {
-	
-// 	})
-// }
+
 
 function start() {
 	$(".question_panel").hide();
-	$(".reset").hide();
 	$(".progress").hide();
 	$(".start").on("click", function() {
-		$("#front_page").fadeOut(300);
-		$(".question_panel").fadeIn(1500);
+		$("#front_page").fadeOut(500);
+		$(".question_panel").fadeIn(1000);
 		$(".next").hide();
 		
 	});
@@ -196,6 +181,7 @@ function start() {
 	userOptions();
 	submitChoice();
 	updateContent();
+	//addContent();
 }
 
 $(function () {
